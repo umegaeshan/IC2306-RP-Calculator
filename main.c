0b4aeb9077e398e1b9eab3afa6fcf9271e08e35e
Umega Eshan <umegaeshan@gmail.com>
2026-05-13 20:22:11 +0530

    Add push function

--- main.c
+++ main.c
@@ -5,3 +5,11 @@
 
 double stack[MAX_STACK_SIZE];
 int top = -1;
+
+void push(double val) {
+    if (top >= MAX_STACK_SIZE - 1) {
+        printf("Error: Stack Overflow\n");
+        return;
+    }
+    stack[++top] = val;
+}
